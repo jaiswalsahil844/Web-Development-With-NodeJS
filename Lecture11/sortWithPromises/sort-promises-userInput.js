9476ffcf4a6713d2425798dbde1f1a82476814e9
SjSahil <sahiljaiswal225@gamil.com>
2020-02-17 19:09:19 +0530

    lecture11

--- Lecture11/sortWithPromises/sort-promises-userInput.js
+++ Lecture11/sortWithPromises/sort-promises-userInput.js
@@ -1,21 +1,44 @@
-const readline = require('readline');
+// const readline = require('readline');
+// const fs = require('fs');
+// const os = require('os');
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question('Enter array of num ', (answer) => {
+
+//     console.log(answer);
+    
+//     var sorted = answer.split(' ').sort((a,b) => a - b).join(os.EOL);
+
+//     fs.writeFile(__dirname + '/myText.txt', sorted, (err) => {
+//         if (err) throw err;
+//         console.log('successfully written');
+//     })
+//     rl.close();
+// });
+
+var standard_input = process.stdin;
+
 const fs = require('fs');
 const os = require('os');
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+standard_input.setEncoding('utf-8');
 
-rl.question('Enter array of num ', (answer) => {
+var input = "";
+standard_input.on('data', (data) =>{
 
-    console.log(answer);
-    
-    var sorted = answer.split(' ').sort((a,b) => a - b).join(os.EOL);;
+    if(data == 'exit\n'){
+        process.exit();
+    }      
+    else{
+        input += data;
+        var sorted = input.split(os.EOL).sort((a,b) => a - b).join(os.EOL);
+        fs.writeFile(__dirname + '/userInput.txt', sorted , (err) => {
+            if (err) throw err;
+        })        
+    }
+})
 
-    fs.writeFile(__dirname + '/myText.txt', sorted, (err) => {
-        if (err) throw err;
-        console.log('successfully written');
-    })
-    rl.close();
-});
