d1880c3f93a9112fbfef05c91bf12b2c28b842c5
SjSahil <sahiljaiswal225@gamil.com>
2020-02-17 19:09:56 +0530

    lecture13

--- Lecture13/ajax-todo/server.js
+++ Lecture13/ajax-todo/server.js
@@ -1,24 +1,41 @@
 const express = require('express')
 const app = express()
 
+const fs = require('fs')
+
 let todos = [
-    'some tasks',
-    'more tasks',
-    'another sample task'
+    // 'some tasks',
+    // 'more tasks',
+    // 'another sample task'
 ]
 
 app.use('/', express.static(__dirname + '/public'))
 
 app.get('/todos', (req, res) => {
+    
+    fs.readFile(__dirname + '/userInput.txt', (err,data) => {
+        if (err) throw err;
+        // console.log(data.toString());
+
+        todos = [];
+        var tasks = data.toString().split(',');
+
+        for(let task of tasks){
+            todos.push(task);
+        }
+    })
+
     res.send(todos)
 })
 
 app.get('/todos/add', (req, res) => {
     todos.push(req.query.task)
+    fs.writeFile(__dirname + '/userInput.txt', todos , (err) => {
+        if (err) throw err;
+    })
     res.send(todos)
 })
 
-
-app.listen(3456, () => {
-    console.log('started on http://localhost:3456')
+app.listen(3458, () => {
+    console.log('started on http://localhost:3458')
 })
